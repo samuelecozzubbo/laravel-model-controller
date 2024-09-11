ba92bc17fad9a3a55a9fdfdfc41c90f4ecb13afc
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-11 16:53:43 +0200

    fix: home style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,22 @@
 
 @section('content')
     <div class="container my-5 home">
-        <h1>{{ $title }}</h1>
+        <div class="jumbotron">
+            <h1 class="display-4">{{ $title }}</h1>
+            <p class="lead">{{ $text }}</p>
+            <hr class="my-4">
+            <p>Clicca qui per vedere l'intero catalogo</p>
+            <p class="lead">
+                <button class="btn btn-warning"><a href="{{ route('movieList') }}" class="text-black text-decoration-none">Vai
+                        ai
+                        film</a></button>
+            </p>
+        </div>
+        <h1></h1>
         <p>
-            {{ $text }}
+
         </p>
-        <button class="btn btn-warning"><a href="{{ route('movieList') }}" class="text-black text-decoration-none">Vai ai
-                film</a></button>
+
     </div>
 @endsection
 
